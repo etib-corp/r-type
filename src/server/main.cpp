04a944fd20a2c8e58e13a68f4ee13f1ce13abf47
Léo Sautron <leo.sautron@epitech.eu>
2024-09-16 19:55:24 +0400

    feat: start use axio in server

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -5,7 +5,16 @@
 ** main.cpp
 */
 
+#include <iostream>
+#include <boost/asio.hpp>
+
 int main(int argc, char **argv)
 {
+    boost::asio::io_context io_context;
+    boost::asio::ip::tcp::acceptor acceptor(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 8080));
+    boost::asio::ip::tcp::socket socket(io_context);
+    acceptor.accept(socket);
+    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
