3add3b330b2120d8088250a73cdb8d2977b79680
ju-lit-hein <julien.ferdinand@epitech.eu>
2024-09-17 23:30:27 +0400

    feat: add Clock class

---
+++ graphical/src/Clock.cpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2024
+** graphical
+** File description:
+** Clock
+*/
+
+#include "Clock.hpp"
+
+LE::Clock::Clock() : _lastFrame(std::chrono::high_resolution_clock::now()) {}
+
+LE::Clock::~Clock() {}
+
+std::size_t LE::Clock::restart()
+{
+    auto now = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - _lastFrame).count();
+    _lastFrame = now;
+    return elapsed;
+}
+
+std::size_t LE::Clock::getElapsedTime() const
+{
+    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - _lastFrame).count();
+}
