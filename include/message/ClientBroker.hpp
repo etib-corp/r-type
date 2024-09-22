95ba66f5ef05f186ecc90a313a3b216ebc296441
Clément Antoine XAVIER <clement-antoine.xavier@epitech.eu>
2024-09-22 18:57:56 +0400

    Fix: Remove bad lines

--- include/message/ClientBroker.hpp
+++ include/message/ClientBroker.hpp
@@ -26,7 +26,6 @@ public:
 
     ~ClientBroker(void)
     {
-        delete _client;
     }
 
     void addMessage(std::uint32_t ecs_id, const std::string &topic_name, std::unique_ptr<Message> message) override;
--- include/message/ServerBroker.hpp
+++ include/message/ServerBroker.hpp
@@ -25,7 +25,6 @@ public:
 
     ~ServerBroker(void)
     {
-        delete _network_module;
     }
 
     void addMessage(std::uint32_t ecs_id, const std::string &topic_name, std::unique_ptr<Message> message) override;
