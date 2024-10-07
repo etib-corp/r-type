7b295a905c2b92d9c3a2ca6e23db9f36f7037cae
Hugo <hugo.payet@epitech.eu>
2024-10-07 23:37:35 +0400

    Feat: adding Enum Class

---
+++ include/EnumClass.hpp
@@ -0,0 +1,40 @@
+/*
+** EPITECH PROJECT, 2024
+** r-type
+** File description:
+** EnumClass
+*/
+
+#ifndef ENUMCLASS_HPP_
+#define ENUMCLASS_HPP_
+
+#include <cstdint>
+
+enum class EcsMovement {
+    UP      = 0,
+    DOWN    = 1,
+    RIGHT   = 2,
+    LEFT    = 3
+};
+
+enum class Axes3D {
+    X       = 0,
+    Y       = 1,
+    Z       = 2,
+};
+
+enum class ActionCode : std::uint8_t {
+    NEW_CONNECTION  = 0x01,
+    INPUT           = 0x02,
+    Rollback        = 0x56,
+    UPDATE_ECS      = 0x57,
+    USERNAME        = 0xAB,
+    UP              = 0xAC,
+    DOWN            = 0xAD,
+    RIGHT           = 0xAE,
+    LEFT            = 0xAF,
+    A = 0x3E,
+    MAGIC_NUMBER    = 0xFF
+};
+
+#endif /* !ENUMCLASS_HPP_ */
