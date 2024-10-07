5eee8a50a94d40163385fcd1bd13d0ff0961403f
Hugo <hugo.payet@epitech.eu>
2024-10-07 23:35:18 +0400

    Feat: adding Common header file

---
+++ include/Common.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** r-type
+** File description:
+** Common
+*/
+
+#ifndef COMMON_HPP_
+#define COMMON_HPP_
+
+#include <cstdint>
+
+struct UpdateEcs {
+    std::uint8_t ecs_id;
+    std::uint8_t actionInput;
+};
+
+#endif /* !COMMON_HPP_ */
