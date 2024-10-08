85fa989731bc7ba29823f5921d964fccb8103eab
Clément Antoine XAVIER <clement-antoine.xavier@epitech.eu>
2024-10-09 00:45:20 +0400

    Fix: Remove compression for now '

--- include/PackUnpack.hpp
+++ include/PackUnpack.hpp
@@ -26,84 +26,93 @@ public:
     ~PackUnpack() = delete;
     [[nodiscard]] static std::vector<uint8_t> pack(const std::vector<uint8_t> &data) noexcept
     {
-        namespace io = boost::iostreams;
-        std::vector<uint8_t> compressedData;
+        // namespace io = boost::iostreams;
+        // std::vector<uint8_t> compressedData;
 
-        try
-        {
-            // Input stream to hold the original data
-            std::stringstream inputStream;
-            inputStream.write(reinterpret_cast<const char *>(data.data()), data.size());
+        // try
+        // {
+        //     // Input stream to hold the original data
+        //     std::stringstream inputStream;
+        //     inputStream.write(reinterpret_cast<const char *>(data.data()), data.size());
 
-            // Output stream to hold compressed data
-            std::stringstream outputStream;
+        //     // Output stream to hold compressed data
+        //     std::stringstream outputStream;
 
-            // Create filtering stream buffer for compression
-            io::filtering_streambuf<io::output> out;
-            out.push(io::zlib_compressor());
-            out.push(outputStream);
+        //     // Create filtering stream buffer for compression
+        //     io::filtering_streambuf<io::output> out;
+        //     out.push(io::zlib_compressor());
+        //     out.push(outputStream);
 
-            // Compress data
-            io::copy(inputStream, out);
+        //     // Compress data
+        //     io::copy(inputStream, out);
+        //     io::close(out);
 
-            // Convert the output stream into a vector of bytes
-            std::string compressedStr = outputStream.str();
-            compressedData.assign(compressedStr.begin(), compressedStr.end());
-        }
-        catch (const boost::iostreams::zlib_error &e)
-        {
-            std::cerr << "Zlib compression error: " << e.what() << " (Error code: " << e.error() << ")" << std::endl;
-        }
-        catch (const std::exception &e)
-        {
-            std::cerr << "General error during packing: " << e.what() << std::endl;
-        }
+        //     // Convert the output stream into a vector of bytes
+        //     std::string compressedStr = outputStream.str();
+        //     compressedData.assign(compressedStr.begin(), compressedStr.end());
+        // }
+        // catch (const boost::iostreams::zlib_error &e)
+        // {
+        //     std::cerr << "Error code: " << e.code().value()
+        //               << " (" << e.code().message() << ")\n"
+        //               << "Error category: " << e.code().category().name() << '\n';
+        // }
+        // catch (const std::exception &e)
+        // {
+        //     std::cerr << "General error during packing: " << e.what() << std::endl;
+        // }
 
-        return compressedData;
+        // return compressedData;
+        return data;
     }
 
     [[nodiscard]] static std::vector<uint8_t> unPack(const std::vector<uint8_t> &compressedData) noexcept
     {
-        namespace io = boost::iostreams;
-        std::vector<uint8_t> decompressedData;
+        // namespace io = boost::iostreams;
+        // std::vector<uint8_t> decompressedData;
 
-        if (compressedData.empty())
-        {
-            std::cerr << "Error: No data to decompress." << std::endl;
-            return decompressedData; // Return empty vector if there's nothing to decompress
-        }
+        // if (compressedData.empty())
+        // {
+        //     std::cerr << "Error: No data to decompress." << std::endl;
+        //     return decompressedData; // Return empty vector if there's nothing to decompress
+        // }
 
-        try
-        {
-            // Input stream to hold the compressed data
-            std::stringstream inputStream;
-            inputStream.write(reinterpret_cast<const char *>(compressedData.data()), compressedData.size());
+        // try
+        // {
+        //     // Input stream to hold the compressed data
+        //     std::stringstream inputStream;
+        //     inputStream.write(reinterpret_cast<const char *>(compressedData.data()), compressedData.size());
 
-            // Output stream to hold decompressed data
-            std::stringstream outputStream;
+        //     // Output stream to hold decompressed data
+        //     std::stringstream outputStream;
 
-            // Create filtering stream buffer for decompression
-            io::filtering_streambuf<io::input> in;
-            in.push(io::zlib_decompressor());
-            in.push(inputStream);
+        //     // Create filtering stream buffer for decompression
+        //     io::filtering_streambuf<io::input> in;
+        //     in.push(io::zlib_decompressor());
+        //     in.push(inputStream);
 
-            // Decompress data
-            io::copy(in, outputStream);
+        //     // Decompress data
+        //     io::copy(in, outputStream);
+        //     io::close(in);
 
-            // Convert the output stream into a vector of bytes
-            std::string decompressedStr = outputStream.str();
-            decompressedData.assign(decompressedStr.begin(), decompressedStr.end());
-        }
-        catch (const boost::iostreams::zlib_error &e)
-        {
-            std::cerr << "Zlib decompression error: " << e.what() << " (Error code: " << e.error() << ")" << std::endl;
-        }
-        catch (const std::exception &e)
-        {
-            std::cerr << "General error during unpacking: " << e.what() << std::endl;
-        }
+        //     // Convert the output stream into a vector of bytes
+        //     std::string decompressedStr = outputStream.str();
+        //     decompressedData.assign(decompressedStr.begin(), decompressedStr.end());
+        // }
+        // catch (const boost::iostreams::zlib_error &e)
+        // {
+        //     std::cerr << "Error code: " << e.code().value()
+        //               << " (" << e.code().message() << ")\n"
+        //               << "Error category: " << e.code().category().name() << '\n';
+                    
+        // }
+        // catch (const std::exception &e)
+        // {
+        //     std::cerr << "General error during unpacking: " << e.what() << std::endl;
+        // }
 
-        return decompressedData;
+        // return decompressedData;
+        return compressedData;
     }
     template <typename T>
     [[nodiscard]] static std::vector<uint8_t> serialize(const T &obj)
