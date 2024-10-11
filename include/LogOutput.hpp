2201500c37a6980887d05de7bfabb3d08cd332a2
Hugo <hugo.payet@epitech.eu>
2024-10-11 13:59:30 +0400

    Feat: adding Enum Class Logger

---
+++ include/LogOutput.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** r-type
+** File description:
+** LogOutput
+*/
+
+#ifndef LOGOUTPUT_HPP_
+#define LOGOUTPUT_HPP_
+
+enum class LogOutput {
+    STDOUT  = 0,
+    STDERR  = 1,
+    FILE    = 2
+};
+
+#endif /* !LOGOUTPUT_HPP_ */
