68cebb504c788ee22258b3dde6ddf8e4246af1c2
Clément Antoine XAVIER <clement-antoine.xavier@epitech.eu>
2024-10-11 11:22:56 +0400

    Feat: Add params to send type in Message

--- src/message/Message.cpp
+++ src/message/Message.cpp
@@ -8,17 +8,16 @@
 #include "message/Message.hpp"
 #include <iomanip>
 
-Message::Message(void) : _emmiter_id(0), _receiver_id(0), _topic_id(0), _action(0)
+Message::Message(void) : _emmiter_id(0), _receiver_id(0), _topic_id(0), _action(0), _is_relialbe(false)
 {
-    memset(&_body, 0, sizeof(Body));
 }
 
 static std::string serializeRequest(Request &request)
 {
     std::ostringstream oss;
 
-    oss.write(reinterpret_cast<const char*>(&request.header), sizeof(request.header));
-    oss.write(reinterpret_cast<const char*>(&request.body), sizeof(uint8_t) * request.header.BodyLength);
+    oss.write(reinterpret_cast<const char *>(&request.header), sizeof(request.header));
+    oss.write(reinterpret_cast<const char *>(&request.body), sizeof(uint8_t) * request.header.BodyLength);
     return oss.str();
 }
 
@@ -46,10 +45,11 @@ std::string Message::serialize(void) const
     return compressAndPrepare(header, _body);
 }
 
-std::ostream &operator<<(std::ostream &stream, const Message &message) {
+std::ostream &operator<<(std::ostream &stream, const Message &message)
+{
     stream << "EmmiterID: " << (unsigned int)message.getEmmiterID() << std::endl;
     stream << "ReceiverID: " << (unsigned int)message.getReceiverID() << std::endl;
-    stream << "TopicID: " <<(unsigned int) message.getTopicID() << std::endl;
+    stream << "TopicID: " << (unsigned int)message.getTopicID() << std::endl;
     stream << "Action: " << (unsigned int)message.getAction() << std::endl;
     return stream;
 }
