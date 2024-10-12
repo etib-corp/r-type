0ecc3e6d5227fc969675207de7e4d75a7811d677
Hugo <hugo.payet@epitech.eu>
2024-10-12 14:46:24 +0400

    Chlore: test method example for fmt lib

---
+++ src/Utils.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** r-type
+** File description:
+** asChar
+*/
+
+#include <iostream>
+#include <fmt/core.h>
+// template <typename Enumeration>
+// auto asChar(Enumeration const value) -> typename std::underlying_type<Enumeration>::type
+// {
+//     return static_cast<typename std::underlying_type<Enumeration>::type>(value);
+// }
+
+int utils() {
+  fmt::print("Hello, world!\n");
+}
