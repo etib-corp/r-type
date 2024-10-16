62600c5ef439f1326e23be430c967a49bdffc2cb
SizzleUnrlsd <hugopayet97411@gmail.com>
2024-10-16 16:01:34 +0400

    Fix: compilation fix by replacing the old version of the attributes with the new one, fix: adding Constructor macro for WIN32

--- include/globalLogger.hpp
+++ include/globalLogger.hpp
@@ -10,7 +10,33 @@
 
 #include "Logger.hpp"
 
-void initLogger(void) __attribute__((constructor));
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef __cplusplus
+    #define INITIALIZER(f) \
+        static void f(void); \
+        struct f##_t_ { f##_t_(void) { f(); } }; static f##_t_ f##_; \
+        static void f(void)
+#elif defined(_MSC_VER)
+    #pragma section(".CRT$XCU",read)
+    #define INITIALIZER2_(f,p) \
+        static void f(void); \
+        __declspec(allocate(".CRT$XCU")) void (*f##_)(void) = f; \
+        __pragma(comment(linker,"/include:" p #f "_")) \
+        static void f(void)
+    #ifdef _WIN64
+        #define INITIALIZER(f) INITIALIZER2_(f,"")
+    #else
+        #define INITIALIZER(f) INITIALIZER2_(f,"_")
+    #endif
+#else
+    #define INITIALIZER(f) \
+        static void f(void) __attribute__((constructor)); \
+        static void f(void)
+#endif
+
+void initLogger(void) [[constructor]];
 
 extern Logger *rtypeLog;
 
