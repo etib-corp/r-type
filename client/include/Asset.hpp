6d3de58a36d1c1228134d5eafc2feae4d333941e
Clément Antoine XAVIER <clement-antoine.xavier@epitech.eu>
2024-10-24 15:27:04 +0400

    Feat: Add get functions

--- client/include/Asset.hpp
+++ client/include/Asset.hpp
@@ -23,7 +23,7 @@ public:
      * @brief Constructor for the Asset class.
      * @param name The name of the asset.
      */
-    Asset(std::string name, std::string file_path) : _name(name), _file_path(file_path) {}
+    Asset(std::string name) : _name(name) {}
 
     /**
      * @brief Destructor for the Asset class.
@@ -34,13 +34,7 @@ public:
      * @brief Gets the name of the asset.
      * @return The name of the asset.
      */
-    const std::string &getName(void) const { return _name; }
-
-    /**
-     * @brief Gets the file path of the asset.
-     * @return The file path of the asset.
-     */
-    const std::string getFilePath(void) const { return _file_path; }
+    std::string getName(void) const { return _name; }
 
     /**
      * @brief Loads the asset.
@@ -49,7 +43,6 @@ public:
 
 protected:
     std::string _name;
-    std::string _file_path;
 
     std::string _read_file(std::string file_path) {
         std::ifstream file(file_path);
@@ -73,70 +66,91 @@ protected:
 class ConfigAsset : public Asset
 {
 public:
-    ConfigAsset(std::string name, std::string file_path) : Asset(name, file_path)
+    ConfigAsset(std::string name, std::string file_path) : Asset(name)
     {
-        if (!_file_path.ends_with(".json"))
+        _config_file = file_path;
+
+        if (!_config_file.ends_with(".json"))
             throw std::runtime_error("Unsupported file format");
     }
     ~ConfigAsset(void) override = default;
 
     void load(void) override
     {
-        std::cout << "Loading file: " << _file_path << std::endl;
-        _config_data = _read_file(_file_path);
+        std::cout << "Loading file: " << _config_file << std::endl;
+        _config_data = _read_file(_config_file);
     }
 
+    std::string getConfigFile(void) const { return _config_file; }
+
+    std::string getConfigData(void) const { return _config_data; }
+
 private:
+    std::string _config_file;
     std::string _config_data;
 };
 
 class FontAsset : public Asset
 {
 public:
-    FontAsset(std::string name, std::string file_path) : Asset(name, file_path)
+    FontAsset(std::string name, std::string file_path) : Asset(name)
     {
-        if (!_file_path.ends_with(".ttf"))
+        _font_file = file_path;
+
+        if (!_font_file.ends_with(".ttf"))
             throw std::runtime_error("Unsupported file format");
     }
     ~FontAsset(void) override = default;
 
     void load(void) override
     {
-        std::cout << "Loading file: " << _file_path << std::endl;
-        _font_data = _read_file_binary(_file_path);
+        std::cout << "Loading file: " << _font_file << std::endl;
+        _font_data = _read_file_binary(_font_file);
     }
 
+    std::string getFontFile(void) const { return _font_file; }
+
+    std::vector<std::uint8_t> getFontData(void) const { return _font_data; }
+
 private:
+    std::string _font_file;
     std::vector<std::uint8_t> _font_data;
 };
 
 class ImageAsset : public Asset
 {
 public:
-    ImageAsset(std::string name, std::string file_path) : Asset(name, file_path)
+    ImageAsset(std::string name, std::string file_path) : Asset(name)
     {
-        if (!_file_path.ends_with(".png"))
+        _image_file = file_path;
+
+        if (!_image_file.ends_with(".png"))
             throw std::runtime_error("Unsupported file format");
     }
     ~ImageAsset(void) override = default;
 
     void load(void) override
     {
-        std::cout << "Loading file: " << _file_path << std::endl;
-        _image_data = _read_file_binary(_file_path);
+        std::cout << "Loading file: " << _image_file << std::endl;
+        _image_data = _read_file_binary(_image_file);
     }
 
+    std::string getImageFile(void) const { return _image_file; }
+
+    std::vector<std::uint8_t> getImageData(void) const { return _image_data; }
+
 private:
+    std::string _image_file;
     std::vector<std::uint8_t> _image_data;
 };
 
 class ModelAsset : public Asset
 {
 public:
-    ModelAsset(std::string name, std::string file_path) : Asset(name, file_path)
+    ModelAsset(std::string name, std::string file_path) : Asset(name)
     {
-        _model_file = _get_material_file_path();
-        _material_file = _get_material_file_path();
+        _model_file = _get_material_file_path(file_path);
+        _material_file = _get_material_file_path(file_path);
 
         if (!std::filesystem::exists(_model_file))
             throw std::runtime_error("Model file: " + _model_file + " not found");
@@ -155,28 +169,36 @@ public:
         _material_data = _read_file(_material_file);
     }
 
+    std::string getModelFile(void) const { return _model_file; }
+
+    std::string getMaterialFile(void) const { return _material_file; }
+
+    std::string getModelData(void) const { return _model_data; }
+
+    std::string getMaterialData(void) const { return _material_data; }
+
 private:
     std::string _model_file;
     std::string _material_file;
     std::string _model_data;
     std::string _material_data;
 
-    std::string _get_model_file_path(void) {
-        return _file_path + "/" + _name + ".obj";
+    std::string _get_model_file_path(std::string file_path) {
+        return file_path + "/" + _name + ".obj";
     }
 
-    std::string _get_material_file_path(void) {
-        return _file_path + "/" + _name + ".mtl";
+    std::string _get_material_file_path(std::string file_path) {
+        return file_path + "/" + _name + ".mtl";
     }
 };
 
 class ShaderAsset : public Asset
 {
 public:
-    ShaderAsset(std::string name, std::string file_path) : Asset(name, file_path)
+    ShaderAsset(std::string name, std::string file_path) : Asset(name)
     {
-        _fragment_file = _get_fragment_file_path();
-        _vertice_file = _get_vertice_file_path();
+        _fragment_file = _get_fragment_file_path(file_path);
+        _vertice_file = _get_vertice_file_path(file_path);
 
         if (!std::filesystem::exists(_fragment_file))
             throw std::runtime_error("Fragment file: " + _fragment_file + " not found");
@@ -195,39 +217,54 @@ public:
         _vertice_data = _read_file(_vertice_file);
     }
 
+    std::string getFragmentFile(void) const { return _fragment_file; }
+
+    std::string getVerticeFile(void) const { return _vertice_file; }
+
+    std::string getFragmentData(void) const { return _fragment_data; }
+
+    std::string getVerticeData(void) const { return _vertice_data; }
+
 private:
     std::string _fragment_file;
     std::string _vertice_file;
     std::string _fragment_data;
     std::string _vertice_data;
 
-    std::string _get_fragment_file_path(void) {
-        return _file_path + "/" + _name + ".frag";
+    std::string _get_fragment_file_path(std::string file_path) {
+        return file_path + "/" + _name + ".frag";
     }
 
-    std::string _get_vertice_file_path(void) {
-        return _file_path + "/" + _name + ".vert";
+    std::string _get_vertice_file_path(std::string file_path) {
+        return file_path + "/" + _name + ".vert";
     }
 };
 
 class SoundAsset : public Asset
 {
 public:
-    SoundAsset(std::string name, std::string file_path) : Asset(name, file_path)
+    SoundAsset(std::string name, std::string file_path) : Asset(name)
     {
-        if (!_file_path.ends_with(".wav") && !_file_path.ends_with(".mp3"))
+        _sound_file = file_path;
+
+        if (!_sound_file.ends_with(".wav") && !_sound_file.ends_with(".mp3"))
             throw std::runtime_error("Unsupported file format");
     }
     ~SoundAsset(void) override = default;
 
     void load(void) override
     {
-        std::cout << "Loading file: " << _file_path << std::endl;
+        std::cout << "Loading file: " << _sound_file << std::endl;
 
-        _sound_data = _read_file_binary(_file_path);
+        _sound_data = _read_file_binary(_sound_file);
     }
 
+    std::string getSoundFile(void) const { return _sound_file; }
+
+    std::vector<std::uint8_t> getSoundData(void) const { return _sound_data; }
+
 private:
+    std::string _sound_file;
     std::vector<std::uint8_t> _sound_data;
 };
 
