d83f62dd61c155668dc500609dcc5e8c6d2721c9
Hugo <hugo.payet@epitech.eu>
2024-11-05 19:45:10 +0400

    Feat: integration of lua

---
+++ serv.conf.lua
@@ -0,0 +1,7 @@
+
+servConfiguration =  {
+    gameEnginePath = "/home/hugo/tek3/r-type-ref/build/dependencies/lion-engine/lib/SFML/liblion-engine-SFML.so",
+    networkModulePath = "./build/dependencies/lion-engine/lib/NetworkModule/libNetworkModule.so",
+    FramerateLimit = 60,
+    Debug = false
+}
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -143,14 +143,159 @@ class ServerGame : public LE::IGame
         int _nbrPlayer;
 };
 
-int main()
+int _main()
 {
-    LE::GraphicalLib lib("/home/julithein/delivery/tek3/r-type/build/dependencies/lion-engine/lib/SFML/liblion-engine-SFML.so");
+    LE::GraphicalLib lib("/home/hugo/tek3/r-type-ref/build/dependencies/lion-engine/lib/SFML/liblion-engine-SFML.so");
     g_engine = lib.createEngine();
 
-    g_engine->init();
+    // g_engine->init();
 
     g_engine->setFramerateLimit(60);
+
+    // g_engine->setGame<ServerGame>();
+
+    // auto scene = g_engine->addScene<LE::Scene>("Default");
+
+    // scene->getEcs()->createEntity();
+
+    // g_engine->playScene("Default");
+
+    // g_engine->run(true);
+    return 0;
+}
+
+#include "luacpp/luacpp.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Enemy {
+public:
+    std::string texture_obj;
+    std::vector<int> start_pos;
+    std::vector<int> end_pos;
+    float speed;
+    int life;
+    int damage;
+    std::string pattern;
+    int scale;
+    std::vector<int> rotation;
+
+    struct Box {
+        int height;
+        int width;
+        std::vector<int> layers;
+        std::vector<int> masks;
+    };
+
+    Box hurtbox;
+    Box hitbox;
+
+    void Print() const {
+        std::cout << "Enemy:\n";
+        std::cout << "  texture_obj: " << texture_obj << "\n";
+        std::cout << "  start_pos: [" << start_pos[0] << ", " << start_pos[1] << ", " << start_pos[2] << "]\n";
+        std::cout << "  end_pos: [" << end_pos[0] << ", " << end_pos[1] << ", " << end_pos[2] << "]\n";
+        std::cout << "  speed: " << speed << "\n";
+        std::cout << "  life: " << life << "\n";
+        std::cout << "  damage: " << damage << "\n";
+        std::cout << "  pattern: " << pattern << "\n";
+        std::cout << "  scale: " << scale << "\n";
+        std::cout << "  rotation: [" << rotation[0] << ", " << rotation[1] << ", " << rotation[2] << "]\n";
+        std::cout << "  hurtbox: { height: " << hurtbox.height << ", width: " << hurtbox.width << ", layers:" << hurtbox.layers[0] << " , mask:" << hurtbox.masks[0] << " }\n";
+        std::cout << "  hitbox: { height: " << hitbox.height << ", width: " << hitbox.width << " }\n";
+    }
+};
+
+std::vector<int> ConvertLuaTableToVector(const luacpp::LuaTable& luaTable) {
+    std::vector<int> result;
+
+    luaTable.ForEach([&result](const luacpp::LuaObject& key, const luacpp::LuaObject& value) -> bool {
+        if (value.GetType() == LUA_TNUMBER) {
+            result.push_back(value.ToInteger());
+            std::cout << "ToInteger= " << value.ToInteger() << std::endl;
+        }
+        return true;
+    });
+
+    return result;
+}
+
+Enemy CreateEnemyFromLua(const luacpp::LuaTable& enemyTable) {
+    Enemy enemy;
+
+    // Charger les attributs de base
+    enemy.texture_obj = enemyTable.Get("texture_obj").ToString();
+
+    luacpp::LuaTable startPosTable = enemyTable.Get("start_pos");
+    startPosTable.ForEach([&enemy](const luacpp::LuaObject& key, const luacpp::LuaObject& value) -> bool {
+        enemy.start_pos.push_back(value.ToInteger());
+        return true;
+    });
+
+    luacpp::LuaTable endPosTable = enemyTable.Get("end_pos");
+    endPosTable.ForEach([&enemy](const luacpp::LuaObject& key, const luacpp::LuaObject& value) -> bool {
+        enemy.end_pos.push_back(value.ToInteger());
+        return true;
+    });
+
+    luacpp::LuaTable rotationTable = enemyTable.Get("rotation");
+    rotationTable.ForEach([&enemy](const luacpp::LuaObject& key, const luacpp::LuaObject& value) -> bool {
+        enemy.rotation.push_back(value.ToInteger());
+        return true;
+    });
+
+    enemy.speed = enemyTable.Get("speed").ToNumber();
+    enemy.life = enemyTable.Get("life").ToInteger();
+    enemy.damage = enemyTable.Get("damage").ToInteger();
+    enemy.pattern = enemyTable.Get("pattern").ToString();
+    enemy.scale = enemyTable.Get("scale").ToInteger();
+
+    // Charger les sous-tables (hurtbox et hitbox)
+    luacpp::LuaTable hurtboxTable = enemyTable.Get("hurtbox");
+    enemy.hurtbox.height = hurtboxTable.Get("height").ToInteger();
+    enemy.hurtbox.width = hurtboxTable.Get("width").ToInteger();
+    luacpp::LuaTable hurtboxLayersTables = hurtboxTable.Get("layers");
+    enemy.hurtbox.layers = ConvertLuaTableToVector(hurtboxLayersTables);
+    // enemy.hurtbox.masks =  ConvertLuaTableToVector(hurtboxTable.Get("masks").ToPointer());
+
+    // Convertir les objets en LuaTable avant l'itération
+    luacpp::LuaTable hurtboxLayersTable = hurtboxTable.Get("layers");
+    hurtboxLayersTable.ForEach([&enemy](const luacpp::LuaObject& key, const luacpp::LuaObject& value) -> bool {
+        enemy.hurtbox.layers.push_back(value.ToInteger());
+        return true;
+    });
+
+    luacpp::LuaTable hurtboxMasksTable = hurtboxTable.Get("masks");
+    hurtboxMasksTable.ForEach([&enemy](const luacpp::LuaObject& key, const luacpp::LuaObject& value) -> bool {
+        enemy.hurtbox.masks.push_back(value.ToInteger());
+        return true;
+    });
+
+    luacpp::LuaTable hitboxTable = enemyTable.Get("hitbox");
+    enemy.hitbox.height = hitboxTable.Get("height").ToInteger();
+    enemy.hitbox.width = hitboxTable.Get("width").ToInteger();
+
+    luacpp::LuaTable hitboxLayersTable = hitboxTable.Get("layers");
+    hitboxLayersTable.ForEach([&enemy](const luacpp::LuaObject& key, const luacpp::LuaObject& value) -> bool {
+        enemy.hitbox.layers.push_back(value.ToInteger());
+        return true;
+    });
+
+    luacpp::LuaTable hitboxMasksTable = hitboxTable.Get("masks");
+    hitboxMasksTable.ForEach([&enemy](const luacpp::LuaObject& key, const luacpp::LuaObject& value) -> bool {
+        enemy.hitbox.masks.push_back(value.ToInteger());
+        return true;
+    });
+
+    return enemy;
+}
+
+#include <vector>
+
+int main(int ac, char **av)
+{
+    g_engine = instanciateEngine();
 
     g_engine->setGame<ServerGame>();
 
@@ -161,5 +306,6 @@ int main()
     g_engine->playScene("Default");
 
     g_engine->run(true);
+
     return 0;
 }
