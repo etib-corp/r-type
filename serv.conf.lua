a5670038cafdcdf84957f245fb83857b75762a29
Léo Sautron <leo.sautron@epitech.eu>
2024-11-05 20:02:03 +0400

    feat: add debug

--- serv.conf.lua
+++ serv.conf.lua
@@ -1,7 +1,7 @@
 
 servConfiguration =  {
-    gameEnginePath = "/home/hugo/tek3/r-type-ref/build/dependencies/lion-engine/lib/SFML/liblion-engine-SFML.so",
-    networkModulePath = "./build/dependencies/lion-engine/lib/NetworkModule/libNetworkModule.so",
+    gameEnginePath = "/home/sleo/etib/r-type/build/dependencies/lion-engine/lib/SFML/liblion-engine-SFML.so",
+    networkModulePath = "/home/sleo/etib/r-type/build/dependencies/lion-engine/lib/NetworkModule/libNetworkModule.so",
     FramerateLimit = 60,
-    Debug = false
+    Debug = true
 }
